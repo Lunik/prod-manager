0d2108c4fda2dce660f60283f3bead65f8e1edce
Lunik <lunik@tiwabbit.fr>
2022-06-17 00:48:56 +0200

    Improve Kubernetes manifests
    feat(deploy.Kubernetes): Add `PodAntiAffinity` and `PodAffinity` on proxy/app deployment (#29)
    feat(deploy.Kubernetes): Add `RollingUpdate` strategy on proxy/app deployment
    feat(deploy.Kubernetes): Add `securityContext` strategy on app deployment
    feat(deploy.Kubernetes): Add `probes` strategy on proxy/app deployment
    fix(deploy.Kubernetes): Fix database persistence volume
    
    Signed-off-by: Lunik <lunik@tiwabbit.fr>

--- ProdManager/routes/health/views.py
+++ ProdManager/routes/health/views.py
@@ -3,14 +3,6 @@ from flask import Blueprint
 
 bp = Blueprint("health", __name__)
 
-@bp.route("/startup", methods=("GET",))
-def startup():
-  return "OK", 200
-
-@bp.route("/readiness", methods=("GET",))
-def readiness():
-  return "OK", 200
-
-@bp.route("/liveness", methods=("GET",))
-def liveness():
+@bp.route("", methods=("GET",))
+def probe():
   return "OK", 200
